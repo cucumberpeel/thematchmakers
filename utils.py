afa8fc9477d5bd3c7a4fd151b682d364ea619220
cucumberpeel <amorygao@gmail.com>
2025-12-02 15:32:26 -0500

    load dtt data

--- utils.py
+++ utils.py
@@ -9,11 +9,54 @@ def load_benchmark_data(
     gold: Optional[str] = None
 ) -> List[Dict]:
     """
-    Loads AutoFJ benchmark data from dataset name.
+    Loads AutoFJ benchmark dataset from dataset name.
     Returns list of dicts with source value, gold value, and list of target values.
+
+    Args:
+        name: Name of the dataset
+        source: Name of the source column
+        target: Name of the target column
+        gold: Name of the gold column
     """
     try:
         raw_data = pd.read_csv(os.path.join("data", name, "gt.csv"))
+    except Exception as e:
+        print(f"Error loading data from {name}: {e}")
+        return []
+    
+    if gold is None:
+        gold = target
+    
+    all_targets = raw_data[target].unique().tolist()
+    
+    dataset = []
+    for _, row in raw_data.iterrows():
+        dataset.append({
+            'source': row[source],
+            'gold': row[gold],
+            'targets': all_targets,
+        })
+    
+    return dataset
+
+def load_dtt_data(
+    name: str,
+    source: str,
+    target: str,
+    gold: Optional[str] = None
+) -> List[Dict]:
+    """
+    Loads DTT dataset from dataset name.
+    Returns list of dicts with source value, gold value, and list of target values.
+
+    Args:
+        name: Name of the dataset
+        source: Name of the source column. Required due to unique column names.
+        target: Name of the target column. Required due to unique column names.
+        gold: Name of the gold column
+    """
+    try:
+        raw_data = pd.read_csv(os.path.join("data", name, "ground truth.csv"))
     except Exception as e:
         print(f"Error loading data from {name}: {e}")
         return []
