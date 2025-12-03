37d0d95d91e5bf3164262339170182e88c9a63ed
cucumberpeel <amorygao@gmail.com>
2025-12-03 15:44:44 -0500

    iterate over all datasets

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from typing import List, Dict, Optional
 
-def load_benchmark_data(
+def load_autofj_data(
     name: str,
     source: str = 'title_l',
     target: str = 'title_r',
@@ -32,17 +32,19 @@ def load_benchmark_data(
     dataset = []
     for _, row in raw_data.iterrows():
         dataset.append({
-            'source': row[source],
-            'gold': row[gold],
-            'targets': all_targets,
-        })
+                'source_column': source,
+                'target_column': target,
+                'source_value': row[source],
+                'gold_value': row[target],
+                'target_values': all_targets,
+            })
     
     return dataset
 
 def load_dtt_data(
     name: str,
-    source: str,
-    target: str,
+    source: Optional[str] = None,
+    target: Optional[str] = None,
     gold: Optional[str] = None
 ) -> List[Dict]:
     """
@@ -51,15 +53,24 @@ def load_dtt_data(
 
     Args:
         name: Name of the dataset
-        source: Name of the source column. Required due to unique column names.
-        target: Name of the target column. Required due to unique column names.
+        source: Name of the source column
+        target: Name of the target column
         gold: Name of the gold column
     """
     try:
-        raw_data = pd.read_csv(os.path.join("data", "FF_AJ", name, "ground truth.csv"))
+        raw_data = pd.read_csv(os.path.join("data", "DXF", name, "ground truth.csv"))
     except Exception as e:
         print(f"Error loading data from {name}: {e}")
         return []
+    
+    if source is None or target is None:
+        source_cols = [col for col in raw_data.columns if col.startswith("source")]
+        target_cols = [col for col in raw_data.columns if col.startswith("target")]
+        if len(source_cols) > 1 or len(target_cols) > 1:
+            print(f"Error locating source-target columns: {raw_data.columns}")
+            return []
+        source = source_cols[0]
+        target = target_cols[0]
     
     if gold is None:
         gold = target
@@ -69,9 +80,11 @@ def load_dtt_data(
     dataset = []
     for _, row in raw_data.iterrows():
         dataset.append({
-            'source': row[source],
-            'gold': row[gold],
-            'targets': all_targets,
-        })
+                'source_column': source,
+                'target_column': target,
+                'source_value': row[source],
+                'gold_value': row[target],
+                'target_values': all_targets,
+            })
     
     return dataset
